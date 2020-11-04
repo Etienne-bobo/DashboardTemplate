0d63ecf4949a535d24e06c196189701bc63eb518
Etienne-bobo <etiennebobo17@gmail.com>
2020-11-04 14:32:42 +0530

    debug

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -6,7 +6,7 @@
               <div class="flex">
 
                 <!-- mobile hamburger -->
-                <div class="inline-block lg:hidden items-center mr-4">
+                <div class="inline-block lg:hidden items-center mr-4 pt-5">
                   <button class="hover:text-blue-500 hover:border-white focus:outline-none navbar-burger" @click="toggleSidebar()">
                     <svg class="h-5 w-5" v-bind:style="{ fill: 'black' }" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Menu</title><path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"/></svg>
                   </button>
